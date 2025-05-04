fa77a2607a7ddf00b392909bcb7f2c4f60a182ec
YK <ykko21@gmail.com>
2025-05-04 11:06:35 -0400

    init

---
+++ src/main/java/org/ykko/HelloworldApplication.java
@@ -0,0 +1,20 @@
+package org.ykko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class HelloworldApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(HelloworldApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+		System.out.println("Hello, World");
+		log.info("Hello, World");
+	}
+
+}
